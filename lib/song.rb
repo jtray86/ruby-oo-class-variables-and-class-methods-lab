010a340a9d8509a06199b99f190c2f1c31dedb00
jennifer tracy <jenniferetracy@gmail.com>
2020-12-11 16:28:25 -0500

    commit tree be95b1172522ab770c55974d9555cd727056b07e

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+require "pry"
+class Song
+    attr_accessor :name, :artist, :genre
+
+@@count = 0
+@@genres = []
+@@artists = []
+def initialize(name, artist, genre)
+    @name =name
+    @artist = artist
+    @genres = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+
+    end
+ def self.count
+    @@count
+ end
+ def self.genre
+    unique_genres = genres.uniq
+ end
+ def self.artists
+ end
+
+end    
+
+binding.pry
+0
